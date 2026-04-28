21e056d21c933b8f2fc105b01b3e63c8f9a79be2
zyh <han20051122@gmail.com>
2026-04-28 23:47:07 +0800

    python_函数

---
+++ Python/第五章/函数基础.py
@@ -0,0 +1,23 @@
+# def out_line():
+#     print("----------------------")
+# out_line()
+# def circle_area(r):
+#     area=3.14*r*r
+#     return area
+# c_area=circle_area(10)
+# print(c_area)
+# def rectangle_area(l,w):
+#     area=l*w
+#     print(area)
+#     return area
+# rectangle_area(20,10)
+
+def triangle_area(b,h):
+        return b*h/2
+        return  b * h /2
+def count_aeiou(s):
+    count=0
+    for ch in s:
+        if ch in 'aeiou':
+            count+=1
+    return count
---
+++ Python/第五章/函数进阶.py
@@ -0,0 +1,31 @@
+# def calc_data(*args):
+#     min_data=min(args)
+#     max_data=max(args)
+#     avg_data=sum(args)/len(args)
+#     return min_data,max_data,avg_data
+#
+# print(calc_data(2,7,9,10,45))
+
+# def add(x,y):
+#     return x+y
+# def calc(x,y,oper):
+#     return oper(x,y)
+# result = calc(10,20,add)
+# print(result)
+
+# out_line=lambda : print("--------------")
+# add=lambda x,y:x+y
+# print(add(1,2))
+#
+# date_list=["C++","Java","Python","Go","PHP","C#","JavaScript"]
+# date_list.sort(key =lambda item : len(item))
+# print(date_list)
+
+# def jc(n):
+#     if n==1 or n==0:
+#         return 1
+#     else:
+#         return n*jc(n-1)
+# n=int(input("请输入数字: "))
+# print(jc(n))
+
